edf446c0b4019e8203a0f166fada8a0064bbbd25
Matthew <mrmcp197@gmail.com>
2026-06-24 09:42:16 -0700

    Selection box bug fix

--- rise-of-the-west/scripts/scene-scripts/selection_box.gd
+++ rise-of-the-west/scripts/scene-scripts/selection_box.gd
@@ -62,7 +62,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		units_in_box.append(area)
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	if selected.has(area) and visible:
+	if area is Unit and visible:
 		deselect_unit(area as Unit)
 		units_in_box.erase(area)
 
