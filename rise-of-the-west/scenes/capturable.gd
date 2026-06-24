b1e074cb429e52234104d31e815c7cd30f819b67
Matthew <mrmcp197@gmail.com>
2026-06-23 18:10:26 -0700

    Capturable Controller

--- rise-of-the-west/scenes/capturable.gd
+++ rise-of-the-west/scenes/capturable.gd
@@ -1,12 +1,17 @@
+class_name capturable
 extends NinePatchRect
 
 @export var value: int = 100
+@export var faction: String = "neutral"
 
 @export var units_in_box: Array[Unit] = []
+
 
 var _light = load("res://assets/CapturableAssets/lightbox.png")
 var _dark = load("res://assets/CapturableAssets/darkbox.png")
 var _neutral = load("res://assets/CapturableAssets/neutralbox.png")
+
+signal faction_change(faction: String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,13 +22,15 @@ func _ready() -> void:
 func _on_timer_timeout() -> void:
 	if value < 200:
 		var unit_count: int = units_in_box.size()
-		var potential = value + unit_count
+		var potential = value + unit_count * 3
 		if potential < 200.0:
-			print(value)
+			if unit_count > 0:
+				print(value)
 			value = potential
 		else:
 			value = 200.0
-			print("captured")
+			faction = "player"
+			faction_change.emit(faction)
 			self.texture = _light
 			$Timer.stop()
 		# Optional: Cast to int for printing if you don't want decimals in your logs
@@ -33,11 +40,9 @@ func _on_timer_timeout() -> void:
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area is Unit and visible:
-		print("unit left capturable zone")
 		units_in_box.erase(area)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is Unit and visible:
-		print("unit entered capturable zone")
 		units_in_box.append(area)
---
+++ rise-of-the-west/scenes/capturable_controller.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var _hold_time = 10
+var _capturable_count = 0
+var _player_capturables = 0
+@onready var capturables: Array[capturable] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children():
+		if child is capturable:
+			if child.has_signal("faction_change"):
+				child.faction_change.connect(_on_capturable_faction_change)
+			_capturable_count += 1
+			capturables.append(child)
+	print("Needed points: ", _capturable_count)
+
+func _on_capturable_faction_change(faction: String) -> void:
+	print(faction)
+	if faction == "player":
+		_player_capturables += 1
+	if _player_capturables == _capturable_count:
+		print("Win")
