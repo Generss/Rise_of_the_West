0cfe336291131ef2e3229073020f6906f5114064
Generss <joshaclark530@gmail.com>
2026-06-25 05:14:16 -0700

    line of sight logic added

--- rise-of-the-west/scripts/scene-scripts/Units/unit_body.gd
+++ rise-of-the-west/scripts/scene-scripts/Units/unit_body.gd
@@ -1,11 +1,17 @@
 class_name UnitBody
 extends RigidBody2D
 
-var speed: float = 200.0
+@export var speed: float = 200.0
 var steering_strength: float = 6.0
 var max_steering_force: float = 1200.0
 
+
+
+@export var vision_range: float = 250.0
+
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var vision_shape: CollisionShape2D = $RangeArea/CollisionShape2D
+@onready var range_area: Area2D = $RangeArea
 
 
 func _ready() -> void:
@@ -13,6 +19,8 @@ func _ready() -> void:
 	lock_rotation = true
 	linear_damp = 4.5
 	can_sleep = false
+	var circle := vision_shape.shape as CircleShape2D
+	circle.radius = vision_range
 
 
 func _physics_process(_delta: float) -> void:
@@ -48,3 +56,22 @@ func die() -> void:
 func _on_unit_movement_initiated(new_target: Vector2) -> void:
 	sleeping = false
 	navigation_agent.target_position = new_target
+
+
+func _on_check_enemies_timer_timeout() -> void:
+	var in_range: Array[Area2D] = range_area.get_overlapping_areas()
+	# so we don't have to create a raycast node
+	var space_state := get_world_2d().direct_space_state
+	for area in in_range:
+		if area is Enemy:
+			var enemy: Enemy = area as Enemy
+			var query : PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create( global_position, enemy.global_position)
+			query.exclude = [self]
+			query.collision_mask = 2
+			var res := space_state.intersect_ray(query)
+			
+			if res.is_empty():
+				print("LINE OF SIGHT")
+			
+			
+			
