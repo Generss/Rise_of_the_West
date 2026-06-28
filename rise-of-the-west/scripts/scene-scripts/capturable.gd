07c14757ceab14416542a9d72cee1d07a8a5bd17
Matthew <mrmcp197@gmail.com>
2026-06-28 11:59:38 -0700

    Enemy Recruitment Types

--- rise-of-the-west/scripts/scene-scripts/capturable.gd
+++ rise-of-the-west/scripts/scene-scripts/capturable.gd
@@ -23,6 +23,9 @@ var cannon_scene = load("res://scenes/Units/unit_cannon.tscn")
 var gatling_scene = load("res://scenes/Units/unit_gatling.tscn")
 @onready var _sortable_node: Node2D = %Sortable
 
+var fort_choices : Array[PackedScene] = [soldier_scene, rifle_scene, cannon_scene, gatling_scene]
+var town_choices : Array[PackedScene] = [soldier_scene, cavalry_scene]
+
 signal faction_change(location: capturable)
 
 @export var economyui : Node
@@ -31,9 +34,15 @@ signal faction_change(location: capturable)
 func _ready() -> void:
 	$Timer.start()
 	$ProgressBar.value = value
-	return # Remove this to restrict recruitment
-	if type != "Fort":
-		get_node("CannonRecruitment").visible = false
+	if type == "Fort":
+		%CannonRecruitment.visible = true
+		%GatlingRecruitment.visible = true
+		%RifleRecruitment.visible = true
+	if type == "Town":
+		%CavalryRecruitment.visible = true
+	if type == "Mine":
+		%TNTRecruitment.visible = true
+		%PistolRecruitment.visible = false
 
 # Should only emit faction change signal WHEN IT CHANGES
 func _on_timer_timeout() -> void:
@@ -83,15 +92,14 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 			enemy_units_in_box.append(area)
 
 func enemy_recruitment() -> void: 
-	if !%EconomyUI.enemy_spend(100):
-		return
-	var soldier_instance: UnitBody = soldier_scene.instantiate()
-	var dimensions: Vector2 = size
-	var random_variation = Vector2(randi_range(0, dimensions.x), randi_range(0, dimensions.y))
-	soldier_instance.position = global_position + random_variation
-	soldier_instance.faction = "Enemy"
-	soldier_instance.economyui = economyui
-	_sortable_node.add_child(soldier_instance)
+	var choice: PackedScene
+	if type == "Fort":
+		choice = fort_choices.pick_random()
+	elif type == "Town":
+		choice = town_choices.pick_random()
+	elif type == "Mine":
+		choice = tnt_scene
+	_recruit(choice, true)
 
 func _on_recruitment_gui_input(event: InputEvent) -> void:
 	_recruit_handle(event, soldier_scene)
@@ -130,8 +138,14 @@ func _recruit(scene: PackedScene, enemy: bool = false):
 			price = 1000
 		gatling_scene:
 			price = 1200
-	if !%EconomyUI.spend(price):
-		return
+	if !enemy:
+		print("ally")
+		if !%EconomyUI.spend(price):
+			return
+	else:
+		price = 100 # Handicap for enemy, otherwise they'll never recruit advanced units
+		if !%EconomyUI.enemy_spend(price):
+			return
 	var instance = scene.instantiate()
 	var dimensions: Vector2 = size
 	var random_variation = Vector2(randi_range(0, dimensions.x), randi_range(0, dimensions.y))
@@ -139,11 +153,13 @@ func _recruit(scene: PackedScene, enemy: bool = false):
 	instance.economyui = economyui
 	if enemy:
 		instance.faction = "Enemy"
+	else:
+		instance.faction = "Ally"
 	_sortable_node.add_child(instance)
 
 func _recruit_handle(event: InputEvent, scene:PackedScene) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.shift_pressed:
 			for i in range(9):
-				_recruit(scene)
-		_recruit(scene)
+				_recruit(scene, false)
+		_recruit(scene, false)
